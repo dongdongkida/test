e5fc3dd82ac82e46d7a621cdc663aff973989701
dongdongkida <48269620+dongdongkida@users.noreply.github.com>
2019-10-17 22:57:49 -0400

    Update Main.java

--- Bank/src/Main.java
+++ Bank/src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	// write your code here
         Bank newBank = new Bank("Bank of America");
         Branch branchMD = new Branch("Maryland Branch");
+	//set up the banks
 
     }
 }
